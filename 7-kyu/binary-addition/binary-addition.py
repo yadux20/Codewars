915587ad0033b3930e8a780a9cc082515bd6afb9
Yadunandan B C <118126992+yadux20@users.noreply.github.com>
2026-03-04 18:32:28 +0530

    Update solution - CodeHub

--- 7-kyu/binary-addition/binary-addition.py
+++ 7-kyu/binary-addition/binary-addition.py
@@ -1,12 +1,9 @@
 def add_binary(a,b):
     tot = a+b
-    bin=''
-    if tot>0:
+    bin = ''
+    if tot > 0:
         while(tot>0):
-            bin+=str(tot%2)
-            tot=tot//2    
+            bin += str(tot%2)
+            tot = tot//2
         return bin[::-1]
-    else:
-        return "0"
-        
-        
+    return 0

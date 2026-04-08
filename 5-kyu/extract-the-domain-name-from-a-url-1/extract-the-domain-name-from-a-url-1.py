6b9c23aa86d3741f912935fe6a3c04ee5fb454ac
Yadunandan B C <118126992+yadux20@users.noreply.github.com>
2026-04-08 23:40:54 +0530

    Update extract-the-domain-name-from-a-url-1.py

--- 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.py
+++ 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.py
@@ -3,5 +3,12 @@ def domain_name(url):
     url = url.replace("www.", "")
     url = url.split("/")[0]
     domain = url.split(".")[0]
-    
     return domain
+
+# def domain_name(url):
+#     headers = ["http://", "https://", "www.", "http://www", "https://www."]
+#     for header in headers:
+#         if header in url:
+#             url = url.replace(header, "")
+#     domain = url.split(".")[0]
+#     return domain

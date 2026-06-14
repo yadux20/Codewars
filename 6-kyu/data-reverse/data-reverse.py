0b6ff2cbcc36c69eab2c394fb8e31e59e9ef9450
Yadunandan B C <118126992+yadux20@users.noreply.github.com>
2026-06-14 22:45:18 +0530

    Update solution - CodeHub

--- 6-kyu/data-reverse/data-reverse.py
+++ 6-kyu/data-reverse/data-reverse.py
@@ -1,9 +1,5 @@
 def data_reverse(data):
-    byt_ls= [data[i:i+8] for i in range(0, len(data),8)]
-    byt_ls.reverse()
-    
     res = []
-    for i in byt_ls:
-        res.extend(i)
-        
+    for i in range(len(data)-8,-1,-8):
+        res.extend(data[i:i+8])
     return res
